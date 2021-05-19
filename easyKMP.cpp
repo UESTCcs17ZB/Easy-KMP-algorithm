90c7bc765edcb714b328a05ab5fdf4735df37238
Zhang <949868575@qq.com>
2021-05-19 11:50:05 +0800

    Update easyKMP.cpp

--- easyKMP.cpp
+++ easyKMP.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 // The following function will calculate the next array of pattern.
 // next[i] is the length of substring(from 0 to i)'s longest prefix and suffix.
-// For example, the next array of pattern "abcabc" will be {0, 0, 1, 2, 3}.
+// For example, the next array of pattern "abcabc" will be {0, 0, 0, 1, 2, 3}.
 // PS: the whole pattern is not a prefix or suffix of itself, thus the next array of "a" is {0}, not {1}!
 void buildNext(const char *pattern, size_t patternLen, size_t *next) {
     next[0] = 0;  // next[0] is always 0, maybe we could optimize this memory unit in the future.
