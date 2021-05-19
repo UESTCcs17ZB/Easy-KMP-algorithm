9070228f6ef6c0b4a10beb2dcf041ab43ac72334
Zhang <949868575@qq.com>
2021-05-19 11:28:50 +0800

    Update easyKMP.cpp

--- easyKMP.cpp
+++ easyKMP.cpp
@@ -1,7 +1,7 @@
 // ----------
 // Author: Zhang Bin, majored in computer science @UESTC, China.
 // ----------
-// I found almost all the codes of KMP algorithm on the Internet were really awful, especially in CSDN, a disgusting plagiarism's website in China.
+// I found almost all the codes of KMP algorithm on the Internet were really awful, especially in CSDN, a plagiarism's disgusting website in China.
 // So I decided to rewrite it for a beautiful one. The easy KMP algorithm, which is really easy to understand and program.
 // No one could write a faster KMP algorithm than mine!
 // ----------
